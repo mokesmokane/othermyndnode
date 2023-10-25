5727b2f7fc7a81f7db24035bdad5be04ff4370ac
mokesmokane <martin_okane@yahoo.com>
2023-10-25 18:12:14 +0100

    src

--- src/server.js
+++ src/server.js
@@ -43,8 +43,10 @@ const firebaseConfig = {
 };
 
 admin.initializeApp({
-  credential: firebaseConfig
+  credential: admin.credential.cert(firebaseConfig)
 });
+
+
 const db = admin.firestore();
 const pinecone = new Pinecone({
   apiKey: pineconeApiKey,
